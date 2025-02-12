c535495000393f7bb535e4902a965a10d988b4af
RodrigoDornelles <rodrigo@dornelles.me>
2025-02-11 22:20:47 -0300

    feat: draw rect mode 1

--- src/tui_draw.c
+++ src/tui_draw.c
@@ -7,10 +7,32 @@ void tui_draw_rect(app_t *const self, int16_t x, int16_t y, int16_t w, int16_t h
     w = MAX(1, w);
     h = MAX(1, h);
 
-    for (uint16_t row = 0; row < h; row++) {
-        concat(self, out,  "\x1b[%d;%dH", row + y, x);
-        memset(&self->out.msg[self->out.len], '#', w);
-        self->out.len += w;
+    if (false) {
+        int16_t x2 = x + w;
+        int16_t y2 = y + h - 1;
+
+        concat(self, out,  "\x1b[%d;%dH+\x1b[%d;%dH+", y, x, y2, x2);
+        concat(self, out,  "\x1b[%d;%dH+\x1b[%d;%dH+", y2, x, y, x2);
+
+        for (uint16_t row = 1; (row + 1) < h; row++) {
+            concat(self, out,  "\x1b[%d;%dH|\x1b[%d;%dH|", y + row, x, y + row, x + w);
+        }
+
+        if (x + 1 < x2) {
+            concat(self, out,  "\x1b[%d;%dH", y, x + 1);
+            memset(&self->out.msg[self->out.len], '-', w - 1);
+            self->out.len += w - 1;
+            concat(self, out,  "\x1b[%d;%dH", y + h - 1, x + 1);
+            memset(&self->out.msg[self->out.len], '-', w - 1);
+            self->out.len += w - 1;
+        }
+    }
+    else {
+        for (uint16_t row = 0; row < h; row++) {
+            concat(self, out,  "\x1b[%d;%dH", row + y, x);
+            memset(&self->out.msg[self->out.len], '#', w);
+            self->out.len += w;
+        }
     }
 }
 
